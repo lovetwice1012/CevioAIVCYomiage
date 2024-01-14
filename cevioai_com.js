9fcb7fb9b401ae397c0d9da307126e973bb7af8a
yussy <lovetwice1012@gmail.com>
2024-01-14 20:05:40 +0900

    Update package.json and cevioai_com.js

--- cevioai_com.js
+++ cevioai_com.js
@@ -5,17 +5,18 @@ const talker = new winax.Object("CeVIO.Talk.RemoteService2.Talker2V40")
 service_control.StartHost(false);
 
 async function getWave(text, character, speed, filename) {
-    talker.Speed = 50 * speed;
+    talker.Speed = 50;
     talker.Volume = 100;
     talker.Tone = 50;
-    talker.ToneScale = 80;
+    talker.ToneScale = 50;
+    console.log(character)
     talker.Cast = character;
-    const wave = await talker.OutputWaveToFile(text, path.join(path.dirname(process.execPath), filename + '.wav'));
+    const wave = await talker.OutputWaveToFile(text, filename);
     return wave;
 }
 
 async function getCastList() {
-    const castList = await talker.AvailableCasts.get();
+    const castList = await talker.AvailableCasts;
     return castList;
 }  
 
--- index.js
+++ index.js
@@ -10,20 +10,19 @@ const {getWave, getCastList}  = require('./cevioai_com.js');
 process.on('unhandledRejection', console.dir);
 process.on('uncaughtException', console.dir);
 
-getCastList().then((castList) => {
-    console.log(castList);
-});
+getCastList()
 
-if(!fs.existsSync(path.dirname(process.execPath) + '/settings.json')){
+if(!fs.existsSync('./settings.json')){
     fs.writeFileSync('./settings.json',JSON.stringify({}));
     console.log('settings.jsonを作成しました。');
 }
 
-const settings = require(path.dirname(process.execPath) + '/settings.json');
+const settings = require('./settings.json');
 
 const settingsTemplate = {
     queue : [],
-    userSettings : {}
+    userSettings : {},
+    disconnectFlag : false,
 }
 
 const userSettingsTemplate = {
@@ -92,6 +91,7 @@ client.on("interactionCreate", async (interaction) => {
             interaction.editReply('すでに接続しています。');
             return;
         }
+        
         const channel = interaction.options.getChannel('channel');
         const connection = joinVoiceChannel({
             channelId: channel.id,
@@ -102,6 +102,10 @@ client.on("interactionCreate", async (interaction) => {
             interaction.editReply('接続しました。');
             if(!settings[interaction.guildId]){
                 settings[interaction.guildId] = settingsTemplate;
+            }
+            if(settings[interaction.guildId].disconnectFlag){
+                settings[interaction.guildId].queue = [];
+                settings[interaction.guildId].disconnectFlag = false;
             }
             let connected_voice = queueTemplate;
             connected_voice.text = '接続しました。';
@@ -120,9 +124,14 @@ client.on("interactionCreate", async (interaction) => {
                     const character = settings[interaction.guildId].userSettings[queue.userId].character;
                     const speed = settings[interaction.guildId].userSettings[queue.userId].speed;
                     console.log(character);
-                    const stream = await getYomiageVoiceStream(queue.text,character,speed,queue.messageId);
+                    const stream = fs.createReadStream("./default/connected.wav");
                     const resource = createAudioResource(stream);
                     player.play(resource);
+                    player.once(AudioPlayerStatus.Idle, () => {
+                        fs.unlink(path.join("./", queue.messageId + '.wav'), (err) => {
+                            
+                        });
+                    })
                 }
             }
 
@@ -139,7 +148,7 @@ client.on("interactionCreate", async (interaction) => {
         connection.subscribe(player);
         player.on(AudioPlayerStatus.Idle, async () => {
             const queues = settings[interaction.guildId].queue;
-            if(queue.length > 0){
+            if(queues.length > 0){
                 const queue = queues.shift();
                 if(!settings[interaction.guildId].userSettings[queue.userId]){
                     settings[interaction.guildId].userSettings[queue.userId] = userSettingsTemplate;
@@ -148,7 +157,12 @@ client.on("interactionCreate", async (interaction) => {
                 const speed = settings[interaction.guildId].userSettings[queue.userId].speed;
                 const stream = await getYomiageVoiceStream(queue.text,character,speed,queue.messageId);
                 const resource = createAudioResource(stream);
-                player.play(resource);
+                await player.play(resource);
+                player.on(AudioPlayerStatus.Idle, () => {
+                    fs.unlink(path.join("./", queue.messageId + '.wav'), (err) => {
+                        
+                    });
+                })
             }
         });
         player.on('error', error => {
@@ -159,7 +173,32 @@ client.on("interactionCreate", async (interaction) => {
     } else if (interaction.commandName === "leave") {
         const connection = getVoiceConnection(interaction.guildId);
         if(connection){
-            connection.destroy();
+            bye = queueTemplate;    
+            bye.text = 'また呼んでねぇ';
+            bye.userId = client.user.id;
+            bye.messageId = 'bye';
+            settings[interaction.guildId].disconnectFlag = true;
+            settings[interaction.guildId].queue = [];
+            settings[interaction.guildId].queue.push(bye);
+            if(settings[interaction.guildId].queue.length === 1){   
+                const connection = getVoiceConnection(interaction.guildId);
+                if(connection){
+                    const player = connection.state.subscription.player;
+                    const queues = settings[interaction.guildId].queue;
+                    const queue = queues.shift();
+                    if(!settings[interaction.guildId].userSettings[queue.userId]){
+                        settings[interaction.guildId].userSettings[queue.userId] = userSettingsTemplate;
+                    }
+                    const character = settings[interaction.guildId].userSettings[queue.userId].character;
+                    const speed = settings[interaction.guildId].userSettings[queue.userId].speed;
+                    const stream = fs.createReadStream("./default/bye.wav");
+                    const resource = createAudioResource(stream);
+                    await player.play(resource);
+                    player.once(AudioPlayerStatus.Idle, () => {
+                        connection.destroy();
+                    }  )
+                }
+            }
             interaction.editReply('切断しました。');
         }else{
             interaction.editReply('接続していません。');
@@ -200,18 +239,18 @@ client.on("interactionCreate", async (interaction) => {
 
 async function getYomiageVoiceStream(text,character,speed,filename){
     return new Promise((resolve, reject) => {
-        let voice = getWave(text, character, speed, path.join(path.dirname(process.execPath), filename + '.wav'))
+        let voice = getWave(text, character, speed, path.join("./", filename + '.wav'))
             if(!voice){
                 reject('voice is false');
             }
-            const stream = fs.createReadStream(path.join(path.dirname(process.execPath), filename + '.wav'));
+            const stream = fs.createReadStream(path.join("./", filename + '.wav'));
             resolve(stream);
     });
 }
 
 client.on('messageCreate', async (message) => {
     if(message.author.bot) return;
-    if(settings[message.guildId] !== undefined){
+    if(settings[message.guildId] !== undefined && settings[message.guildId].disconnectFlag === false){
         let queue = settings[message.guildId].queue;
         let queueData = queueTemplate;
         queueData.text = message.content;
@@ -233,9 +272,14 @@ client.on('messageCreate', async (message) => {
                 const stream = await getYomiageVoiceStream(queue.text,character,queue.speed,queue.messageId);
                 const resource = createAudioResource(stream);
                 player.play(resource);
+                player.once(AudioPlayerStatus.Idle, () => {
+                    fs.unlink(path.join("./", queue.messageId + '.wav'), (err) => {
+                        if (err) throw err;
+                    });
+                })
             }
         }
     }
 });
 
-client.login(config.token);
+client.login('MTE3NjgyODI1NjgzMDM2OTc5Mg.GsJ0fS.c81LB5-PaiY6c2sbwoMk20j3mFuIdc91KIOW0Q');
